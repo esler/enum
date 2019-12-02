929908594159a246f242cd5053d9bbe1bbdb66b2
Ondřej Ešler <ondrej.esler@intraworlds.com>
2019-12-02 23:04:13 +0100

    get rid of circleci

--- .circleci/run-build-locally.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-curl --user ${CIRCLE_TOKEN}: \
-    --request POST \
-    --form revision=9434d298f18b266a169d39542986576f195a00de\
-    --form config=@config.yml \
-    --form notify=false \
-        https://circleci.com/api/v1.1/project/github/intraworlds/enum/tree/master
